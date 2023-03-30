3b454837239c21bb2e885de555e84ba655bf53a0
chen-huai <52665471+chen-huai@users.noreply.github.com>
2023-03-30 21:40:19 +0800

    学习fastapi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -16,3 +17,35 @@ async def say_hello(name: str):
 @app.get("/test")
 async def say_hello():
     return {"message": "test dfasdfasdf"}
+
+
+
+# 定义选择
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+app = FastAPI()
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+# 路径参数带着路径
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+# 声明不属于路径参数的其他函数参数时，它们将被自动解释为"查询字符串"参数
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]

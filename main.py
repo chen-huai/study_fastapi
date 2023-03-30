c039a9d8dbef223e30fbeac9025318138f30ed29
chen-huai <52665471+chen-huai@users.noreply.github.com>
2023-03-30 20:51:26 +0800

    学习fastapi

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/test")
+async def say_hello():
+    return {"message": "test dfasdfasdf"}
---
+++ test.py
@@ -0,0 +1,10 @@
+# # uvicorn test:app --reload
+#
+# from fastapi import FastAPI
+#
+# app = FastAPI()
+#
+#
+# @app.get("/test/{name}")
+# async def test(name: str):
+#     return {"message": f"Hello {name}"}

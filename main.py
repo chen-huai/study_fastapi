64f3cc164b5d82c5e4e0e4f403e7ee8f2cb122b4
chen-huai <52665471+chen-huai@users.noreply.github.com>
2023-03-31 16:12:41 +0800

    学习fastapi

--- main.py
+++ main.py
@@ -4,22 +4,6 @@ from enum import Enum
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.get("/test")
-async def say_hello():
-    return {"message": "test dfasdfasdf"}
-
-
-
 # 定义选择
 class ModelName(str, Enum):
     alexnet = "alexnet"
@@ -49,3 +33,28 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+
+# 首页
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/test")
+async def say_hello():
+    return {"message": "test dfasdfasdf"}
+
+# 在这个例子中，函数参数 q 将是可选的，并且默认值为 None
+from typing import Union
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
